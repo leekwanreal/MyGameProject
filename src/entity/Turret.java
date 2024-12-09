1481f9009d79b887b7138326f62f0a008822c038
HewstoneM <160411418+HewstoneM@users.noreply.github.com>
2024-12-09 23:54:13 +0700

    Update Turret.java
    add key function to turret

--- src/entity/Turret.java
+++ src/entity/Turret.java
@@ -15,11 +15,20 @@ import object.OBJ_Fireball;
 
 public class Turret extends Entity{
 	public int pauseCount = 0;
+	//Hieu
+	//GamePanel gp;
+	//
 	public Turret(GamePanel gp) {
 		super(gp);
-		
+		//Hieu
+		this.gp = gp;
+		//
 		//type = type_monster;
+		type = type_obstacle;
 		name = "Turret";
+		//Hieu
+		collision = true;
+		//
 		speed = 0;
 		maxLife = 600;  
 		life = maxLife;
@@ -27,7 +36,7 @@ public class Turret extends Entity{
 		defense = 222;
 		exp = 0;
 		projectile = new OBJ_Rock(gp);
-		turnOn = true;
+		turnOn = false;
 		onPath = false;
 		Paused = true;
 		
@@ -52,6 +61,12 @@ public class Turret extends Entity{
 		right2 = setup("/npc/secret_weapon_right.png", gp.tileSize*3, gp.tileSize*3);
 	}
 	
+	//Hieu
+	public void interact() {
+		gp.gameState = gp.dialogueState;
+		gp.ui.currentDialogue = "You need a key to activate this weapon.";
+	}
+	//
 	
 	private int i = 0; 
 	protected boolean Paused = false;
@@ -73,9 +88,13 @@ public class Turret extends Entity{
 	        	direction = "up";
 	        	projectile.set(worldX + 54,worldY + 54,direction,true,this);
 	        	gp.projectileList.add(projectile);
+
+	            //gp.projectileList.add(upBullet);
+	            //projectile.subtractResource(this);
 	            shotCoolDown = 0;
 	            i++;
 	            Paused = true;
+	            //turretPause(1);
 	        }
 	        if (i >= 3 && i < 6 && projectile.alive == false && shotCoolDown == 10) {
 	            direction = "right";
@@ -89,21 +108,57 @@ public class Turret extends Entity{
 	            direction = "down";
 	        	projectile.set(worldX + 54, worldY + 54, direction,true,this);
 	        	gp.projectileList.add(projectile);
+
 	            shotCoolDown = 0;
 	            i++;
 	            Paused = true;
+
+	            //turretPause(1);
 	        }
 	        if (i >= 9 && i < 12 && projectile.alive == false && shotCoolDown == 10) {
 	            direction = "left";
 	        	projectile.set(worldX + 54,worldY + 54,direction,true,this);
 	        	gp.projectileList.add(projectile);
+
 	            shotCoolDown = 0;
 	            i++;
 	            Paused = true;
+
 	        }
 	        if (i >= 12) {
 	            i = 0; 
 	        }
+			/*actionLockCounter++;
+			int j = 0;
+			
+			if (actionLockCounter == 120) {
+				Random random = new Random();
+				// Get a number 1 - 100
+				int i = random.nextInt(100) + 1; 
+				if (i <= 25) {
+					direction = "up";
+					j = 0;
+				}
+				if (i > 25 && i <= 50) {
+					direction = "down";
+					j = 0;
+				}
+				if (i > 50 && i <= 75) {
+					direction = "left";
+					j = 0;
+				}
+				if (i > 75 && i <= 100) {
+					direction = "right";
+					j = 0;
+				}
+			}	
+			actionLockCounter = 0;
+		    if (projectile.alive == false && shotCoolDown == 5) {
+			projectile.set(worldX, worldY, direction, true, this);
+			gp.projectileList.add(projectile);
+			projectile.subtractResource(this);
+			shotCoolDown = 0;
+		    }  */  
 	    }			
 	}
 
